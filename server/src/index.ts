0add9f63576618b4b7471d66dd040f15a786b329
Tejas Nayak B <tejasnayakc42@gmail.com>
2024-09-11 22:09:24 +0530

    Minor changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use("/", outlookRouter);
 app.get("/", (req, res) => {
   res.send(`
         Welcome to the Demo!<br>
-        Please Check <a href="https://tejasnayak.tech" target="_blank">my portfolio for further information ;)</a>.
+        Please Check my  <a href="https://tejasnayak.tech" target="_blank">portfolio </a>for further information ;).
       `);
 });
 
